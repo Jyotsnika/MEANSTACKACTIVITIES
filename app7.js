cae321f3454cf07a1829fc507dfc8faa8fcdf960
Jyotsnika <117299569+Jyotsnika@users.noreply.github.com>
2023-09-08 22:00:20 +0530

    Express-mongo-node

---
+++ app7.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const { main } = require('./db7');
+var api=express();
+api.get('/',function(request,response){
+    res.send('Iam a default root');
+});
+api.get('/data',async function(request,response){
+    var result=await main();
+    response.send(result);
+});
+
+api.listen(777,function(){
+    console.log('api server started');
+})
---
+++ db6.js
@@ -0,0 +1,23 @@
+const {MongoClient} = require('mongodb');
+const client=new MongoClient('mongodb://127.0.0.1:27017');
+async function main()
+{
+    
+    await client.connect();
+    console.log('server connected');
+   const db=client.db("makeskilled");
+    console.log('database selected');
+   const collection=db.collection('teamdetails');
+    console.log('collection selected')
+   const result1=await collection.deleteOne({"Name":"Jyotsnika"});
+
+    const result=await collection.find({}).toArray();
+    console.log(result);
+
+    return result;
+
+}
+
+module.exports={
+    main
+}
